508705d60a884abe01891a4b0ed8156ea030e5b3
Gloxinia5 <punciaaron09@gmail.com>
2022-01-23 12:34:34 +0800

    Calculator

--- GUI.py
+++ GUI.py
@@ -1,18 +1,38 @@
 import tkinter as tk
+from tkinter import ttk
 
 root = tk.Tk()
-root.title("GUI")
+title = root.title("Calculator")
 
 # WINDOW ATTRIBUTE
+root.resizable(False, False)
 window_width = 500
 window_height = 500
-root.resizable(False, False)
-screenX = root.winfo_screenwidth()
-screenY = root.winfo_screenheight()
-root.attributes('-topmost', 1)
-root.attributes('-alpha', 0.5)
+root.iconbitmap('./asset/calculator.ico')
+
+# FRAME
+ttk.Label(root, text = 'this is a label placed in root.').pack()
+frame = tk.Frame(root)
+leftFrame = tk.Frame(root)
+leftFrame.pack(side='left')
+rightFrame = tk.Frame(root)
+rightFrame.pack(side='right')
+
+# LABEL
+label1 = ttk.Label(leftFrame, text = 'this is a label placed in left frame.').pack()
+label2 = ttk.Label(rightFrame, text = 'this is a label placed in right frame.').pack()
+
+# CHECK FOR EVENT
+def button_clicked():
+    print(True)
+
+# BUTTON
+bt1 = ttk.Button(root, text = "HELLO WORLD", command = button_clicked)
+bt1.pack()
 
 # CENTER WINDOW
+screenX = root.winfo_screenwidth()
+screenY = root.winfo_screenheight()
 centerX = int(screenX/2 - window_width/2)
 centerY = int(screenY/2 - window_height/2)
 
